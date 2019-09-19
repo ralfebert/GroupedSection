f2695b64db0d830bfd675ed9da733cfe3f33ede7
Ralf Ebert <info@ralfebert.de>
2019-09-19 12:26:05 +0200

    tests + code cleanup

--- Sources/GroupedSection/GroupedSection.swift
+++ Sources/GroupedSection/GroupedSection.swift
@@ -1,40 +1,59 @@
-// Copyright 2018, Ralf Ebert
-// Licensed under: https://opensource.org/licenses/MIT
+// MIT License
+//
+// Copyright (c) 2019 Ralf Ebert
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 import Foundation
 
 public protocol HasRows {
     associatedtype RowItem
-    
-    var rows : [RowItem] { get }
+
+    var rows: [RowItem] { get }
 }
 
-public struct GroupedSection<SectionItem : Comparable&Hashable, RowItem> : Comparable, HasRows {
-    
-    public let sectionItem : SectionItem
-    public let rows : [RowItem]
-    
-    public static func < (lhs: GroupedSection, rhs: GroupedSection) -> Bool {
-        return lhs.sectionItem < rhs.sectionItem
+public struct GroupedSection<SectionItem: Hashable, RowItem>: HasRows {
+
+    public var sectionItem: SectionItem
+    public var rows: [RowItem]
+
+    public static func group(rows: [RowItem], by criteria: (RowItem) -> SectionItem) -> [GroupedSection<SectionItem, RowItem>] {
+        let groups = Dictionary(grouping: rows, by: criteria)
+        return groups.map(GroupedSection.init(sectionItem:rows:))
     }
-    
-    public static func == (lhs: GroupedSection, rhs: GroupedSection) -> Bool {
-        return lhs.sectionItem == rhs.sectionItem
-    }
-    
-    public static func group(rowItems : [RowItem], by criteria : (RowItem) -> SectionItem ) -> [GroupedSection<SectionItem, RowItem>] {
-        let groups = Dictionary(grouping: rowItems, by: criteria)
-        return groups.map(GroupedSection.init(sectionItem:rows:)).sorted()
+
+}
+
+extension GroupedSection: CustomStringConvertible {
+
+    public var description: String {
+        return String(describing: self.sectionItem) + "\n" + self.rows.map { "- \($0)" }.joined(separator: "\n")
     }
 
 }
 
 // see: https://forums.swift.org/t/extension-on-array-where-element-is-generic-type/10225
-extension Array where Element: HasRows {
-    
-    public subscript(indexPath: IndexPath) -> Element.RowItem {
+public extension Array where Element: HasRows {
+
+    subscript(indexPath: IndexPath) -> Element.RowItem {
         assert(indexPath.count == 2)
         return self[indexPath[0]].rows[indexPath[1]]
     }
-    
+
 }
--- Tests/GroupedSectionTests/GroupedSectionTests.swift
+++ Tests/GroupedSectionTests/GroupedSectionTests.swift
@@ -1,15 +1,100 @@
-import XCTest
+// MIT License
+//
+// Copyright (c) 2019 Ralf Ebert
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 @testable import GroupedSection
+import XCTest
+
+private let dateOnlyFormatter: DateFormatter = {
+    let dateFormat = DateFormatter()
+    dateFormat.dateFormat = "yyyy-MM-dd"
+    return dateFormat
+}()
 
 final class GroupedSectionTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        // XCTAssertEqual(GroupedSection().text, "Hello, World!")
+
+    struct Headline {
+        var date: Date
+        var title: String
     }
 
-    static var allTests = [
-        ("testExample", testExample),
+    let headlines = [
+        Headline(date: dateOnlyFormatter.date(from: "2018-05-15")!, title: "Proin suscipit maximus"),
+        Headline(date: dateOnlyFormatter.date(from: "2018-02-15")!, title: "In ac ante sapien"),
+        Headline(date: dateOnlyFormatter.date(from: "2018-03-05")!, title: "Lorem Ipsum"),
+        Headline(date: dateOnlyFormatter.date(from: "2018-02-10")!, title: "Aenean condimentum"),
     ]
+
+    private func firstDayOfMonth(date: Date) -> Date {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month], from: date)
+        return calendar.date(from: components)!
+    }
+
+    func testUseCaseExampleGroupHeadlines() {
+        var sections = GroupedSection.group(rows: self.headlines, by: { firstDayOfMonth(date: $0.date) })
+        sections.sort { lhs, rhs in lhs.sectionItem < rhs.sectionItem }
+
+        let stringSections = sections.map { GroupedSection(sectionItem: dateOnlyFormatter.string(from: $0.sectionItem), rows: $0.rows.map { $0.title }) }
+
+        let result = stringSections.map { $0.description }.joined(separator: "\n\n")
+
+        XCTAssertEqual(result,
+                       """
+                       2018-02-01
+                       - In ac ante sapien
+                       - Aenean condimentum
+
+                       2018-03-01
+                       - Lorem Ipsum
+
+                       2018-05-01
+                       - Proin suscipit maximus
+                       """)
+
+    }
+
+    var exampleSections: [GroupedSection<String, String>] {
+        return [GroupedSection(sectionItem: "A", rows: ["Alice", "Amy"]), GroupedSection(sectionItem: "B", rows: ["Bob", "Bert"])]
+    }
+
+    func testIndexPathSubscript() {
+        let sections = self.exampleSections
+        XCTAssertEqual(sections[IndexPath(row: 0, section: 0)], "Alice")
+        XCTAssertEqual(sections[IndexPath(row: 1, section: 0)], "Amy")
+        XCTAssertEqual(sections[IndexPath(row: 0, section: 1)], "Bob")
+        XCTAssertEqual(sections[IndexPath(row: 1, section: 1)], "Bert")
+    }
+
+    func testDescription() {
+
+        XCTAssertEqual("""
+        A
+        - Alice
+        - Amy
+        
+        B
+        - Bob
+        - Bert
+        """, self.exampleSections.map { $0.description }.joined(separator: "\n\n"))
+    }
+
 }
